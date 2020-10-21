f061daa8ec2d024d75dfe8451afeb71bbdcd0963
Kaitlin Regan <karegan@gmail.com>
2020-10-21 10:54:32 -0400

    Second commit

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,32 +3,42 @@ class CLI
     #Make instance of application here
 
     def run
-        puts "Welcome to the CLI Pick a Movie app!"
+        puts "Welcome to the"
+        puts "
+        +-+-+-+-+-+-+-+-+-+-+-+-+
+        |P|i|c|k|-|a|-|M|o|v|i|e|
+        ++-+-+-++-+-+-+-+-+-+-+-+
+         |A|p|p|                 
+         +-+-+-+                 
+       "
         sleep (1)
         puts "Loading..."
         puts "Done!"
         Scraper.new.first_scrape
         puts "Which genre of movie would you like to watch?"
        input = gets.strip
-        if input == "Action"
+        if input == "1"
             puts "Great! So you'd like to watch an Action movie. Which Action movie would you like more information about?"
             Scraper.new.second_scrape
+            input = gets.strip
+            if input == "1"
+                Scraper.new.dark_knight_info
+            elsif input == "5"
+                Scraper.new.lord_of_rings
+                puts "Press 1 to see more action movies or 2 to return to the main menu."
+                input = gets.strip
+                if input == "1"
+                    Scraper.new.second_scrape
+                elsif input == "2"
+                    run
+                end
+            end
+        elsif input == "2"
+            puts "Great! Adventure movies are awesome! Which would you like to know more about?" 
         else
             puts "Please hold."
         end
-=begin            puts "Here are your choices for action movies:"
-        elsif input == "2"
-            puts "Here are your choices for Comedy movies:"
-        elsif input == "3"
-            puts "Here are your choices for Family movies:"
-        elsif input == "4"
-            puts "Here are your choices for Horror movies:"
-        elsif input == "5"
-            puts "Here are your choices for Thriller movies:"
-        else
-            puts "That is not an option. Please try again."
-        end
-=end
+
     end
 
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,16 +12,21 @@ class Scraper
         genre_elements = html_parsed_to_elements.css('.quicklinks')
         
         genre_elements.each do |movie_genre|
-            genre_name = movie_genre.css("a").text
+            first_genre_name = movie_genre.css("a")[0].text
+            second_genre_name = movie_genre.css("a")[1].text
+            third_genre_name = movie_genre.css("a")[4].text
             #title = movie_genre.css(".titleColumn").text
         
             
             #genre = Genre.find_or_create_by_name(genre_name)
 
-            Movie.new(genre_name)
+         #   Movie.new(genre_name)
 
             puts "Genres: "
-            puts "#{genre_name}"
+            puts "1. #{first_genre_name}"
+            puts "2. #{second_genre_name}"
+            puts "3. #{third_genre_name}"
+
         end
 
     end
@@ -44,6 +49,22 @@ class Scraper
             puts "4. #{fourth_movie_name}"
             puts "5. #{fifth_movie_name}"
             
+        end
+    end
+
+    def dark_knight_info
+
+    end
+
+    def lord_of_rings
+        html = open(@base_url + "/search/title/?genres=action&sort=user_rating,desc&title_type=feature&num_votes=25000,&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=5aab685f-35eb-40f3-95f7-c53f09d542c3&pf_rd_r=9WCAAT5YKX085TKXRD5K&pf_rd_s=right-6&pf_rd_t=15506&pf_rd_i=top&ref_=chttp_gnr_1")
+        html_parsed_to_elements = Nokogiri::HTML(html)
+        lord_desc = html_parsed_to_elements.css('.lister')
+
+        lord_desc.each do |describe|
+            lord_description = describe.css("p")[1].text
+            puts "Description:"
+            puts "#{lord_description}"
         end
     end
  #   binding.pry
